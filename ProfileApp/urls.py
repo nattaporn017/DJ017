1ac2ea2643d2cd7a55520a53c3409ba8a6ce94fe
Acer <nattaporn.bn@rmuti.ac.th>
2023-01-15 14:04:17 +0700

    first commit 15/01

---
+++ ProfileApp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from ProfileApp import views
+
+urlpatterns = [ #สร้างเร้าเตอร์
+    path('', views.home, name = 'home'),
+    path('test', views.test, name = 'test'),
+    path('firstweb', views.firstweb, name = 'firstweb'),
+    path('secondpage', views.secondpage, name = 'secondpage'),
+    path('thirdpage', views.thirdpage, name = 'thirdpage'),
+    path('hny', views.hny, name = 'hny'),
+
+
+
+]
